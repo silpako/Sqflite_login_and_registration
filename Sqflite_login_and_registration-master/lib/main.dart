3536ebb188141014039e5898c630828415afb392
silpako <silpako167@gmail.com>
2024-07-14 00:07:56 +0530

    first commit

---
+++ Sqflite_login_and_registration-master/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:auth_sqlflite/Screens/login_signup.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login_Signup(),
+    );
+  }
+}
